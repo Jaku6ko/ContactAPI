e680bf17833fb1a9d1dee4bd21845a10fbdb0e89
Jakub <jaksad4@gmail.com>
2020-11-28 17:42:19 +0100

    Adding the project

---
+++ ContactApi/Controllers/ContactsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ContactApi.Models;
+using ContactApi.Repositories;
+
+namespace ContactApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        [Route("get")]
+        public ActionResult GetOption(int id)
+        {
+            ContactRepository Repo = new ContactRepository();
+            var result = Repo.Get(id);
+            return Ok(result);
+        }
+
+        [Route("list")]
+        public ActionResult ListOption()
+        {
+            ContactRepository Repo = new ContactRepository();
+            var result = Repo.List();
+            return Ok(result);
+        }
+        [HttpPost]
+        [Route("create")]
+        public ActionResult CreateOption(Contact Model)
+        {
+            ContactRepository Repo = new ContactRepository();
+            Repo.Create(Model);
+            return Ok();
+        }
+        [Route("delete")]
+        public ActionResult DeleteOption(int id)
+        {
+            ContactRepository Repo = new ContactRepository();
+            Repo.Delete(id);
+            return Ok();
+        }
+        [Route("update")]
+        public ActionResult UpdateOption(Contact Model, int id)
+        {
+            ContactRepository Repo = new ContactRepository();
+            Repo.Update(id, Model);
+            return Ok();
+        }
+    }
+}
---
+++ ContactApi/Repositories/ContactRepository.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactApi.Models;
+using System.Data.SqlClient;
+
+namespace ContactApi.Repositories
+{
+    public class ContactRepository
+    {
+        public Contact Get(int id)
+        {
+
+            string commandLine = string.Format("SELECT * FROM contacts WHERE id ={0}", id);
+            var connection = getConnection();
+            connection.Open();
+            var command = new SqlCommand(commandLine, connection);
+            var result = command.ExecuteReader();
+            result.Read();
+            var contact = FillContact(result);
+            connection.Close();
+            return contact;
+        }
+        public List<Contact> List(int Limit = 5)
+        {
+            var contacts = new List<Contact>();
+            string commandLine = string.Format("SELECT TOP({0}) * FROM contacts", Limit);
+            var connection = getConnection();
+            var command = new SqlCommand(commandLine, connection);
+            connection.Open();
+            var result = command.ExecuteReader();
+            while (result.Read())
+            {
+                contacts.Add(FillContact(result));
+            }
+            connection.Close();
+            return contacts;
+        }
+        public void Create(Contact contact)//Method that creates a new contact in the database
+        {
+            string commandLine = string.Format("INSERT INTO contacts VALUES('{0}', '{1}', '{2}')", contact.Name, contact.Email, contact.MobileNumber);
+            var connection = getConnection();
+            var command = new SqlCommand(commandLine, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Delete(int id)
+        {
+            string commandLine = string.Format("DELETE FROM contacts WHERE id = {0} ", id);
+            var connection = getConnection();
+            connection.Open();
+            var command = new SqlCommand(commandLine, connection);
+            var result = command.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void Update(int id, Contact contact)
+        {
+            string commandLine = string.Format("UPDATE contacts SET name = '{1}', email = '{2}', mobile_number = '{3}' WHERE id = {0}",
+                id, contact.Name.ToString(), contact.Email.ToString(), contact.MobileNumber.ToString());
+            var connection = getConnection();
+            connection.Open();
+            var command = new SqlCommand(commandLine, connection);
+            var result = command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+        private SqlConnection getConnection()//Method that establishes connection to the database
+        {
+            return new SqlConnection(@"Data Source=.\SQLEXPRESS;
+                Initial Catalog=contact_database;
+                Integrated Security=True;
+                Connect Timeout=30;
+                Encrypt=False;
+                TrustServerCertificate=False;
+                ApplicationIntent=ReadWrite;
+                MultiSubnetFailover=False"
+                );
+        }
+        public Contact FillContact(SqlDataReader result)//Method that fills the contact variable with database info
+        {
+            var Contact = new Contact();
+            Contact.Id = (int)result.GetValue(0);
+            Contact.Name = (string)result.GetValue(1);
+            Contact.Email = (string)result.GetValue(2);
+            Contact.MobileNumber = (string)result.GetValue(3);
+            return Contact;
+
+        }
+    }
+
+}
